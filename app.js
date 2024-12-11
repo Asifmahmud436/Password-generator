b42514ed9b186b84f2d40faf46011b51ba16f3b0
Zettex <safaandsafa4@gmail.com>
2024-12-11 18:35:51 +0600

    color added to pass

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const generateBtn = document.getElementById("generate-btn");
 generateBtn.addEventListener("click", function () {
   firstPass.textContent = generateRandomPassword();
   secondPass.textContent = generateRandomPassword();
+  if (firstPass.style.color == "magenta" || firstPass.style.color == "white") {
+    firstPass.style.color = "lightgreen";
+  } else {
+    firstPass.style.color = "magenta";
+  }
+  if (
+    secondPass.style.color == "lightgreen" ||
+    secondPass.style.color == "white"
+  ) {
+    secondPass.style.color = "magenta";
+  } else {
+    secondPass.style.color = "lightgreen";
+  }
 });
 
 function generateRandomPassword() {
